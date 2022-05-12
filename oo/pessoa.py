3709af8c90c874f5f5ef9d9bce0139eef6e3b1ac
sandro99999 <sandroptak@gmail.com>
2022-05-12 10:40:33 -0300

    Criado atributo Complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,20 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    Sandro = Pessoa(nome='Sandro')
+    Batata = Pessoa(Sandro, nome='Batata')
+    print(Pessoa.cumprimentar(Batata))
+    print(id(Batata))
+    print(Batata.cumprimentar())
+    print(Batata.nome)
+    print(Batata.idade)
+    for filho in Batata.filhos:
+        print(filho.nome)
